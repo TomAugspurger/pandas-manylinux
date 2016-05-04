a2db5d80282ce4e4b698a5016105f383be686de8
Tom Augspurger <tom.augspurger88@gmail.com>
2016-05-04 08:45:50 -0500

    initial commit

---
+++ travis/build-wheels.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+# Manylinux, openblas version, lex_ver
+source /io/common_vars.sh
+
+# Directory to store wheels
+rm_mkdir unfixed_wheels
+
+# Compile wheels
+for PYBIN in /opt/python/*/bin; do
+    ${PYBIN}/pip install -r /io/dev-requirements.txt
+    ${PYBIN}/pip wheel /io/ -w wheelhouse/
+done
+
+
+for whl in wheelhouse/*.whl; do
+    auditwheel repair $whl -w /io/wheelhouse/
+done
+
+for PYBIN in /opt/python/*/bin/; do
+    ${PYBIN}/pip install python_manylinux_demo --no-index -f /io/wheelhouse
+    (cd $HOME; ${PYBIN}/nosetests pymanylinuxdemo)
+done
