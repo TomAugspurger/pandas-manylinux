c353eb77119a0284d7c01ba3c071e1ddcdfa3518
Tom Augspurger <tom.augspurger88@gmail.com>
2016-05-04 19:16:54 -0500

    skip 26, 33; install pandas

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -6,8 +6,12 @@ mkdir unfixed_wheels
 
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
-    ${PYBIN}/pip install -r /io/dev-requirements.txt
-    ${PYBIN}/pip wheel /io/ -w wheelhouse/
+    if [[ ${PYBIN} != *"26"* ]] && [[ ${PYBIN} != *"33"* ]]; then
+        ${PYBIN}/pip install -r /io/dev-requirements.txt
+        ${PYBIN}/pip wheel /io/ -w wheelhouse/
+    else
+        echo "skipping ${PYBIN}"
+    fi
 done
 
 
@@ -16,6 +20,7 @@ for whl in wheelhouse/*.whl; do
 done
 
 for PYBIN in /opt/python/*/bin/; do
-    ${PYBIN}/pip install python_manylinux_demo --no-index -f /io/wheelhouse
-    (cd $HOME; ${PYBIN}/nosetests pymanylinuxdemo)
+    ${PYBIN}/pip install pandas --no-index -f /io/wheelhouse
+    (cd $HOME; ${PYBIN}/nosetests pandas)
 done
+
