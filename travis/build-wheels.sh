89ea9e2924978e6213cdfe890469b283fd48d095
Tom Augspurger <tom.augspurger88@gmail.com>
2016-06-11 15:11:17 -0500

    try master, use Matthews suggestions.

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -3,15 +3,15 @@ set -e -x
 
 echo ${PYVER}
 
-git clone https://github.com/pydata/pandas /io/pandas
-cd /io/pandas
-git checkout v0.18.1
+git clone https://github.com/pydata/pandas
+cd pandas
+# git checkout v0.18.1
 
 PYBIN=/opt/python/${PYVER}/bin
 # Compile wheels
 echo "building for ${PYBIN}"
-${PYBIN}/pip install -r /io/pandas/ci/requirements_dev.txt
-${PYBIN}/pip wheel /io/pandas/ -w wheelhouse/
+${PYBIN}/pip install -r ci/requirements_dev.txt
+${PYBIN}/pip wheel --no-deps -w wheelhouse/ .
 
 for whl in wheelhouse/pandas*.whl; do
     auditwheel repair $whl -w /io/wheelhouse/
