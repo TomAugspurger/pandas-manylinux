0bc8798a9855972c620b5fcd3c06efa2e65c63ef
Tom Augspurger <tom.augspurger88@gmail.com>
2016-06-11 11:10:09 -0500

    try matrix

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -5,20 +5,16 @@ git clone https://github.com/pydata/pandas /io/pandas
 cd /io/pandas
 git checkout v0.18.1
 
+set PYBIN=/opt/python/${PYVER}/bin
 # Compile wheels
-for PYBIN in /opt/python/*/bin; do
-    if [[ ${PYBIN} != *"26"* ]] && [[ ${PYBIN} != *"33"* ]]; then
-        echo "building for ${PYBIN}"
-        ${PYBIN}/pip install -r /io/pandas/ci/requirements_dev.txt
-        ${PYBIN}/pip wheel /io/pandas/ -w wheelhouse/
-    else
-        echo "skipping ${PYBIN}"
-    fi
-done
-
+echo "building for ${PYBIN}"
+${PYBIN}/pip install -r /io/pandas/ci/requirements_dev.txt
+${PYBIN}/pip wheel /io/pandas/ -w wheelhouse/
 
 for whl in wheelhouse/pandas*.whl; do
-    if [[ ${whl} == *"pandas"* ]]; then
-        auditwheel repair $whl -w /io/wheelhouse/
-    fi
+    auditwheel repair $whl -w /io/wheelhouse/
 done
+
+${PYBIN}/pip install pandas --no-index -f /io/wheelhouse
+cd $HOME; ${PYBIN}/nosetests pandas
+
