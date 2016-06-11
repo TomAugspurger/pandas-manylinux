8ddecc5c03a06051d303c3228106642607a9010a
Matthew Brett <matthew.brett@gmail.com>
2016-06-11 12:51:59 -0700

    Comments on wheel building script
    I think you actively don't want the pandas checkout to go into the host operating system, because you want the checkout to be a temporary copy that you will throw away with the building image.
    
    The `--no-deps` flag avoids copying dependencies into the wheelhouse, such as numpy.

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -e -x
 
-git clone https://github.com/pydata/pandas /io/pandas
-cd /io/pandas
+git clone https://github.com/pydata/pandas
+cd pandas
 git checkout v0.18.1
 
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
     if [[ ${PYBIN} != *"26"* ]] && [[ ${PYBIN} != *"33"* ]]; then
         echo "building for ${PYBIN}"
-        ${PYBIN}/pip install -r /io/pandas/ci/requirements_dev.txt
-        ${PYBIN}/pip wheel /io/pandas/ -w wheelhouse/
+        ${PYBIN}/pip install -r ci/requirements_dev.txt
+        ${PYBIN}/pip wheel --no-deps -w wheelhouse/ .
     else
         echo "skipping ${PYBIN}"
     fi
